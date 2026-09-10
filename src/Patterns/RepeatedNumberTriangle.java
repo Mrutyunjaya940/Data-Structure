80a989515fda7a5ee3b8ad2b2a07a4bb32926dec
Mrutyunjaya Sahoo <sahoo10022004@gmail.com>
2026-09-10 12:29:52 +0530

    solved RepeatedNumberTriangle and Inverted Number Triangle

---
+++ src/Patterns/RepeatedNumberTriangle.java
@@ -0,0 +1,19 @@
+public class RepeatedNumberTriangle {
+
+//            1
+//            22
+//            333
+//            4444
+//            55555
+    public static void main(String[] args) {
+        int num=6;
+        for(int i=1;i<num;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
