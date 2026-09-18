3ea38ec6ee0e4a83c482dd3c8703239396fa5dee
Mrutyunjaya Sahoo <sahoo10022004@gmail.com>
2026-09-18 23:49:46 +0530

    Create NumberPyramid.java

---
+++ src/NumberPyramid.java
@@ -0,0 +1,24 @@
+public class NumberPyramid {
+    //    1
+    //   123
+    //  12345
+    // 1234567
+    //123456789
+
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0;j<n-i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i+1; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+    }
+}
