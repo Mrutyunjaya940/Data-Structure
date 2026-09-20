68aaf43778339439f4d60f66dfa18b757ba5d653
Mrutyunjaya Sahoo <sahoo10022004@gmail.com>
2026-09-21 02:06:57 +0530

    Add ValidPalindrome class to check for palindromes

---
+++ src/ValidPalindrome.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+
+public class ValidPalindrome {
+    public static boolean ispalindrom(String s)
+    {
+        s=s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
+        int i=0;
+        int j=s.length()-1;
+        while (i <= j)
+        {
+            if(s.charAt(i) != s.charAt(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        String check="A man, a plan, a canal: Panama";
+        System.out.println(ispalindrom(check));
+    }
+}
