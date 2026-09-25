45892dc1ef5bb63c1499a210166e85a7fd491b2c
Mrutyunjaya Sahoo <sahoo10022004@gmail.com>
2026-09-25 21:25:18 +0530

    Rename class CombinationUniINt to ComUnionIntersection

---
+++ HashSet/ComUnionIntersection.java
@@ -0,0 +1,26 @@
+package IBM;
+
+import java.util.HashSet;
+
+public class ComUnionIntersection {
+    public static void main(String[] args) {
+        int[] arr1={1,2,3,4};
+        int[] arr2={3,4,5,6};
+
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0; i<arr1.length; i++)
+        {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+
+            if (set.contains(arr2[i])) {
+                set.remove(arr2[i]);
+            }
+            else {
+                set.add(arr2[i]);
+            }
+        }
+        System.out.println(set);
+    }
+}
